036be3e222c9a11b401319b36056e99650226966
Mandroidification <zhangsikai314@mail.zhangsikai.com>
2022-09-23 17:11:06 +0800

    fix db name

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
-const sequelize = new Sequelize("wx", MYSQL_USERNAME, MYSQL_PASSWORD, {
+const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
   host,
   port,
   dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
